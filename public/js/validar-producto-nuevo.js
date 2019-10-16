d11160b4a6818d963227896ce0d8bc574835b9b3
Jonva12 <jonva12@hotmail.com>
2019-10-16 18:55:21 +0200

    cambio validacion

--- public/js/validar-producto-nuevo.js
+++ public/js/validar-producto-nuevo.js
@@ -1,22 +1,73 @@
-function validarProducto() {
-  var nombre = document.getElementById("nombre").value;
-  var descripcion = document.getElementById("descripcion").value;
-  var imagen = document.getElementById("img").value;
-  var stock = document.getElementById("stock").value;
-  var enlace = document.getElementById("enlace").value;
-  var valido = false;
+$(document).ready(function() {
+  let nombreMal = document.getElementById("nombreMal");
+  let descMal = document.getElementById("descMal");
+  let imgMal = document.getElementById("imgMal");
+  let stockMal = document.getElementById("stockMal");
+  let enlaceMal = document.getElementById("enlaceMal");
+  nombreMal.hidden = true;
+  descMal.hidden = true;
+  imgMal.hidden = true;
+  stockMal.hidden = true;
+  enlaceMal.hidden = true;
 
-  if (nombre != "" && descripcion != "" && imagen != "" && stock != "" && enlace != "") {
-    if (descripcion.length <= 256) {
-      valido = true;
-    } else {
-      alert("Descripcion demasiada larga!");
+  $('input').change(function(){
+    comprobar();
+  });
+
+  function comprobar(){
+    var bien = 0;
+    var nombre = document.getElementById("nombre");
+    var desc = document.getElementById("descripcion");
+    var img = document.getElementById("img");
+    var stock = document.getElementById("stock");
+    var enlace = document.getElementById("enlace");
+    if (typeof nombre != "string" && nombre.value.length == 0){
+        nombreMal.hidden = false;
+        bien--;
+    }else{
+      nombreMal.hidden = true;
+      bien++;
     }
-  } else {
-    alert("Se requieren todos los campos!");
+    if(desc.value.length === 0){
+        descMal.hidden = false;
+        bien--;
+    }else{
+      descMal.hidden = true;
+      bien++;
+    }
+    if(img.files.length == 0){
+      imgMal.hidden = false;
+      bien--;
+    }else{
+      imgMal.hidden = true;
+      bien++;
+    }
+    if (typeof stock != "number" && stock.value.length == 0){
+        stockMal.hidden = false;
+        bien--;
+    }else{
+      stockMal.hidden = true;
+      bien++;
+    }
+    if (typeof enlace != "string" && enlace.value.length == 0){
+        enlaceMal.hidden = false;
+        bien--;
+    }else{
+      enlaceMal.hidden = true;
+      bien++;
+    }
+    if(bien === 5){
+      EnableSubmit();
+    }else{
+      DisableSubmit();
+    }
   }
 
-  if (valido) {
-    console.log("subir archivo")
+  function EnableSubmit(){
+    document.getElementById("submit").disabled = false;
   }
-}
+
+  function DisableSubmit(){
+    document.getElementById("submit").disabled = true;
+  }
+});
