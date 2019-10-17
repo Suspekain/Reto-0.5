8e1fa7b86ab7f669f316caaa9f8e6774fba7bdd8
crojaspe18dw <crojaspe18dw@ikzubirimanteo.com>
2019-10-17 10:30:58 +0200

    para que mireis porque el header me sale abajo de la web usando blade

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -2,14 +2,32 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
 </head>
 <body>
-	<header>
-		@yield("header")
-		<img src="" id="logoametzondo">
-		<h1>ametzondo</h1>
-		<img src="" id="candado">
-	</header>
+<header>
+	@yield("header")
+      <img class="ametzondologo" src="{{url('/img/ametzondologo.png')}}" alt="Ametzondo logoa"/>
+      <img class="candado" src="{{url('/img/candado.png')}}" alt="candado"/>
+  </header>
+
+
+
+
+
+
+
+
+
+
+
+    <footer>
+    @yield("footer")
+    <div id="mapa">
+      <iframe src="https://maps.google.com/maps?width=100%&height=600&hl=es&q=Centro%20comercial%20en%20Saint-Pierre-d&apos;Irube%2C%20Francia+(Ametzondo)&ie=UTF8&t=&z=15&iwloc=B&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+      </iframe>
+    </div>
+  </footer>
 
 </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="css/estilo.css">
-
-  <title>Ametzondo</title>
-</head>
+@extends("layouts.plantilla")
 <body>
-  <header>
-      <img class="ametzondologo" src="{{url('/img/ametzondologo.png')}}" alt="Ametzondo logoa"/>
-      <img class="candado" src="{{url('/img/candado.png')}}" alt="candado"/>
-  </header>
+  @section("header")
+
+  @endsection
+
   <section>
     <div class="tienda">
       <h2>Tienda1</h2>
@@ -64,11 +57,10 @@
       </div>
     </div>
   </section>
-  <footer>
-    <div id="mapa">
-      <iframe src="https://maps.google.com/maps?width=100%&height=600&hl=es&q=Centro%20comercial%20en%20Saint-Pierre-d&apos;Irube%2C%20Francia+(Ametzondo)&ie=UTF8&t=&z=15&iwloc=B&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
-      </iframe>
-    </div>
-  </footer>
+  
+  @section("footer")
+
+
+  @endsection
 </body>
 </html>
