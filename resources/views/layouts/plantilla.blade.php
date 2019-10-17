c527948accdb5993d089c5550c87361f98718777
crojaspe18dw <crojaspe18dw@ikzubirimanteo.com>
2019-10-17 09:45:12 +0200

    cambios layouts

---
+++ resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<header>
+		@yield("header")
+		<img src="" id="logoametzondo">
+		<h1>ametzondo</h1>
+		<img src="" id="candado">
+	</header>
+
+</body>
+</html>
