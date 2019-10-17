4af2c879ece5a5ad5d8b667d041480ed0ff345ee
Ekain Susperregi Ibarguren <suspekain@gmail.com>
2019-10-17 08:21:27 +0200

    landing pageakin hasita

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/estilo.css">
+
+  <title>Ametzondo</title>
+</head>
+<body>
+  <h1>Ametzondo</h1>
+  <section>
+    <div class="tienda">
+      <h2>Tienda1</h2>
+      <div class="productos">
+        <div class="producto">
+          <h4>Producto1</h4>
+          <img src="">
+        </div>
+        <div class="producto">
+          <h4>Producto2</h4>
+          <img src="">
+        </div>
+        <div class="producto">
+          <h4>Producto3</h4>
+          <img src="">
+        </div>
+      </div>
+    </div>
+    <div class="tienda">
+      <h2>Tienda1</h2>
+      <div class="productos">
+        <div class="producto">
+          <h4>Producto1</h4>
+          <img src="">
+        </div>
+        <div class="producto">
+          <h4>Producto2</h4>
+          <img src="">
+        </div>
+        <div class="producto">
+          <h4>Producto3</h4>
+          <img src="">
+        </div>
+      </div>
+    </div>
+    <div class="tienda">
+      <h2>Tienda1</h2>
+      <div class="productos">
+        <div class="producto">
+          <h4>Producto1</h4>
+          <img src="">
+        </div>
+        <div class="producto">
+          <h4>Producto2</h4>
+          <img src="">
+        </div>
+        <div class="producto">
+          <h4>Producto3</h4>
+          <img src="">
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer>
+    <div id="mapa">
+      <iframe src="https://maps.google.com/maps?width=100%&height=600&hl=es&q=Centro%20comercial%20en%20Saint-Pierre-d&apos;Irube%2C%20Francia+(Ametzondo)&ie=UTF8&t=&z=15&iwloc=B&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+      </iframe>
+    </div>
+  </footer>
+</body>
+</html>
