e45d08c882b1bc889499ef151c17a9241b353629
Ekain Susperregi Ibarguren <suspekain@gmail.com>
2019-10-17 13:20:18 +0200

    index presentable

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,54 +8,83 @@
     @include("layouts.header")
   </header>
   <section>
+
     <div class="tienda">
-      <h2>Tienda1</h2>
+      <div class="logotienda">
+        <img src="img/tiendas/afflelou.png" alt="alainafflelou">
+      </div>
       <div class="productos">
         <div class="producto">
-          <h4>Producto1</h4>
-          <img src="">
+          <h3>Gafas graduadas</h3>
+          <img class="imagenproducto" src="img/productos/gafas-graduadas.jpg">
         </div>
         <div class="producto">
-          <h4>Producto2</h4>
-          <img src="">
+          <h3>Gafas de sol</h3>
+          <img class="imagenproducto" src="img/productos/gafas-sol.jpg">
         </div>
         <div class="producto">
-          <h4>Producto3</h4>
-          <img src="">
+          <h3>Gafas graduadas</h3>
+          <img class="imagenproducto" src="img/productos/gafas-graduadas2.jpg">
         </div>
       </div>
     </div>
+
     <div class="tienda">
-      <h2>Tienda1</h2>
+      <div class="logotienda">
+        <img src="img/tiendas/carrefour.png" alt="carrefour">
+      </div>
       <div class="productos">
         <div class="producto">
-          <h4>Producto1</h4>
-          <img src="">
+          <h3>Papel</h3>
+          <img class="imagenproducto" src="img/productos/papel.jpg">
         </div>
         <div class="producto">
-          <h4>Producto2</h4>
-          <img src="">
+          <h3>Yogurt</h3>
+          <img class="imagenproducto" src="img/productos/yogutr.jpeg">
         </div>
         <div class="producto">
-          <h4>Producto3</h4>
-          <img src="">
+          <h3>Deterjente</h3>
+          <img class="imagenproducto" src="img/productos/ariel.jpeg">
         </div>
       </div>
     </div>
+
     <div class="tienda">
-      <h2>Tienda1</h2>
+      <div class="logotienda">
+        <img src="img/tiendas/celio.png" alt="celio">
+      </div>
       <div class="productos">
         <div class="producto">
-          <h4>Producto1</h4>
-          <img src="">
+          <h3>Camisa</h3>
+          <img class="imagenproducto" src="img/productos/camisa.jpg">
         </div>
         <div class="producto">
-          <h4>Producto2</h4>
-          <img src="">
+          <h3>Pantalones</h3>
+          <img class="imagenproducto" src="img/productos/pantalones.jpg">
         </div>
         <div class="producto">
-          <h4>Producto3</h4>
-          <img src="">
+          <h3>Polo</h3>
+          <img class="imagenproducto" src="img/productos/polo.jpg">
+        </div>
+      </div>
+    </div>
+
+    <div class="tienda">
+      <div class="logotienda">
+        <img src="img/tiendas/ikea.png" alt="ikea">
+      </div>
+      <div class="productos">
+        <div class="producto">
+          <h3>Kallax</h3>
+          <img class="imagenproducto" src="img/productos/kallax.webp">
+        </div>
+        <div class="producto">
+          <h3>Lisabo</h3>
+          <img class="imagenproducto" src="img/productos/mesalisabo.webp">
+        </div>
+        <div class="producto">
+          <h3>Harry</h3>
+          <img class="imagenproducto" src="img/productos/sillaharry.webp">
         </div>
       </div>
     </div>
