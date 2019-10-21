1474fbfb70a8f583e1de736226494121db7ac09e
Ekain Susperregi Ibarguren <suspekain@gmail.com>
2019-10-21 08:20:56 +0200

    validacion producto

--- resources/views/pages/crear-producto.blade.php
+++ resources/views/pages/crear-producto.blade.php
@@ -2,24 +2,25 @@
 <html>
   <head>
     @include("layouts.head")
+    <script type="text/javascript" src="js/validar-producto-nuevo.js"></script>
   </head>
   <body>
     <header>
       @include("layouts.header")
     </header>
-    <form class="productonuevo" action="form.crear" method="get">
+    <form class="productonuevo" action="" method="get">
       <label>Crear nuevo producto:</label><br><br><br>
       <label>Nombre: </label>
-      <input type="text" maxlength="45"/><br><br><br>
+      <input type="text" maxlength="45" id="nombre"><br><br><br>
       <label>Descripcion: </label><br>
-      <textarea rows="8" cols="80"></textarea><br><br><br>
+      <textarea rows="8" cols="80" id="descripcion"></textarea><br><br><br>
       <label>Imagen: </label>
-      <input type="file"/><br><br><br>
+      <input type="file" id="img"/><br><br><br>
       <label>Stock: </label>
-      <input type="text"/><br><br><br>
+      <input type="number" id="stock"/><br><br><br>
       <label>Enlace</label>
-      <input type="text" value="http://"/><br><br><br>
-      <button type="button" name="button">Crear Producto</button>
+      <input type="text" value="http://" id="enlace"/><br><br><br>
+      <input type="button" onclick="validarProducto()" value="Crear producto">
     </form>
   </body>
 </html>
