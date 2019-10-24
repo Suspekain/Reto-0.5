ec99374dcdfb3116afb98139535df0a93944603d
Ekain Susperregi Ibarguren <suspekain@gmail.com>
2019-10-24 09:04:46 +0200

    cerrar div

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -17,18 +17,19 @@
       <div class="productos">
         <div class="producto">
           @foreach($productos as $producto)
-            @if($producto->ProductoTienda === $tienda->idTienda)
-              <div>{{$producto->ProductoNombre}}</div>
-              <img class="imagenproducto" src="/img/productos/{{$producto->ProductoImagen}}">
-            @endif
+          @if($producto->ProductoTienda === $tienda->idTienda)
+          <div>{{$producto->ProductoNombre}}</div>
+          <img class="imagenproducto" src="/img/productos/{{$producto->ProductoImagen}}">
+          @endif
           @endforeach
         </div>
+      </div>
     </div>
-    <hr>
-    @endforeach
-  </section>
-  <footer>
-    @include("layouts.footer")
-  </footer>
-</body>
-</html>
+      <hr>
+      @endforeach
+    </section>
+    <footer>
+      @include("layouts.footer")
+    </footer>
+  </body>
+  </html>
